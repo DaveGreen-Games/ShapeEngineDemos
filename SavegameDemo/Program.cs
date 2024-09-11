74d4ca3a3958da8c795a6e660deda2c925821fc2
Dave Green <34277803+SoloByte@users.noreply.github.com>
2024-09-11 09:42:48 +0200

    Savegame demo finished.

--- SavegameDemo/Program.cs
+++ SavegameDemo/Program.cs
@@ -2,6 +2,7 @@
 using ShapeEngine.Color;
 using ShapeEngine.Core;
 using ShapeEngine.Core.Structs;
+using ShapeEngine.Input;
 using ShapeEngine.Lib;
 using ShapeEngine.Persistent;
 using ShapeEngine.Random;
@@ -32,29 +33,19 @@ public class MyGame : Game
     private SavegameBasic savegameBasic;
     private SavegameProfile[] savegameProfiles;
     private SavegameProfile currentSavegameProfile;
+    private int currentSavegameProfileIndex;
     private const int maxProfiles = 3;
     private TextFont textFont;
     
-    public static readonly string BasicPath = $"{ShapeSavegame.APPLICATION_DATA_PATH}/savegames";
-    public static readonly string ProfilesPath = $"{ShapeSavegame.APPLICATION_DATA_PATH}/savegames/profiles";
+    public static readonly string BasicPath = Path.Combine(ShapeSavegame.ApplicationDocumentsPath, "savegames");
+    public static readonly string ProfilesPath = Path.Combine(ShapeSavegame.ApplicationDocumentsPath, "savegames/profiles");   
     public MyGame(GameSettings gameSettings, WindowSettings windowSettings) : base(gameSettings, windowSettings)
     {
-        
-        
-        Console.WriteLine($"        > Application Folder: {ShapeSavegame.APPLICATION_DATA_PATH}");
-        Console.WriteLine($"        > Savegame Folder: {BasicPath}");
-        Console.WriteLine($"        > Application Folder: {ProfilesPath}");
-        
-        // Console.WriteLine($"        > Savegame Folder: {SavegameFolder.Path}");
-        // Console.WriteLine($"        > Application Folder: {ProfileFolder.Path}");
-
-        
-        
         var basic = ShapeSavegame.Load<SavegameBasic>(BasicPath, "basic.txt");
         savegameProfiles = new SavegameProfile[maxProfiles];
         if (basic == null)
         {
-            savegameBasic = CreateSavegameBasic("profile1");
+            savegameBasic = CreateSavegameBasic("Profile1");
             ShapeSavegame.Save(savegameBasic, BasicPath, savegameBasic.name);
             
             for (int i = 0; i < savegameProfiles.Length; i++)
@@ -64,19 +55,22 @@ public class MyGame : Game
                 ShapeSavegame.Save(profile, ProfilesPath, profile.name);
             }
             currentSavegameProfile = savegameProfiles[0];
+            currentSavegameProfileIndex = 0;
         }
         else
         {
             savegameBasic = basic;
             for (int i = 0; i < savegameProfiles.Length; i++)
             {
-                var profile = ShapeSavegame.Load<SavegameProfile>(ProfilesPath, $"profile{i + 1}.txt");
+                var fileName = $"profile{i + 1}.txt";
+                var profile = ShapeSavegame.Load<SavegameProfile>(ProfilesPath, fileName);
                 if (profile == null)
                 {
                     var newProfile = CreateSavegameProfile(i);
                     savegameProfiles[i] = newProfile;
                     if (newProfile.ProfileName == savegameBasic.CurrentProfileName)
                     {
+                        currentSavegameProfileIndex = i;
                         currentSavegameProfile = newProfile;
                     }
                 }
@@ -85,6 +79,7 @@ public class MyGame : Game
                     savegameProfiles[i] = profile;
                     if (profile.ProfileName == savegameBasic.CurrentProfileName)
                     {
+                        currentSavegameProfileIndex = i;
                         currentSavegameProfile = profile;
                     }
                 }
@@ -92,12 +87,12 @@ public class MyGame : Game
         }
 
         var font = Raylib.GetFontDefault();
-        textFont = new TextFont(font, 32, 5, 0, new ColorRgba(Color.Beige));
+        textFont = new TextFont(font, 24, 4, -6, new ColorRgba(Color.Beige));
     }
 
     private SavegameBasic CreateSavegameBasic(string profileName)
     {
-        return new(profileName, "basic.txt");
+        return new(profileName);
     }
     private SavegameProfile CreateSavegameProfile(int index)
     {
@@ -119,22 +114,72 @@ public class MyGame : Game
         
     }
 
+    protected override void Update(GameTime time, ScreenInfo game, ScreenInfo gameUi, ScreenInfo ui)
+    {
+        if (ShapeKeyboardButton.ESCAPE.GetInputState().Pressed)
+        {
+            Quit();
+            return;
+        }
+        
+        if (ShapeKeyboardButton.SPACE.GetInputState().Pressed) NextProfile();
+        if (ShapeKeyboardButton.Q.GetInputState().Pressed) RandomizeCurrentProfile();
+        if (ShapeKeyboardButton.R.GetInputState().Pressed) ResetSavegames();
+    }
+
+    private void ResetSavegames()
+    {
+        savegameBasic = CreateSavegameBasic("Profile1");
+        ShapeSavegame.Save(savegameBasic, BasicPath, savegameBasic.name);
+            
+        for (int i = 0; i < savegameProfiles.Length; i++)
+        {
+            var profile = CreateSavegameProfile(i);
+            savegameProfiles[i] = profile;
+            ShapeSavegame.Save(profile, ProfilesPath, profile.name);
+        }
+        currentSavegameProfile = savegameProfiles[0];
+        currentSavegameProfileIndex = 0;
+    }
+
+    private void RandomizeCurrentProfile()
+    {
+        // currentSavegameProfile.Age = Rng.Instance.RandI(0, 100);
+        currentSavegameProfile.Chance = Rng.Instance.RandF();
+        ShapeSavegame.Save(currentSavegameProfile, ProfilesPath, currentSavegameProfile.name);
+    }
+
+    private void NextProfile()
+    {
+        currentSavegameProfileIndex += 1;
+        if(currentSavegameProfileIndex >= maxProfiles) currentSavegameProfileIndex = 0;
+        currentSavegameProfile = savegameProfiles[currentSavegameProfileIndex];
+        savegameBasic.CurrentProfileName = currentSavegameProfile.ProfileName;
+        ShapeSavegame.Save(savegameBasic, BasicPath, savegameBasic.name);
+    }
+
     protected override void DrawGame(ScreenInfo game)
     {
         
         
         game.Area.Draw(new ColorRgba(Color.DarkOliveGreen));
-        game.Area.DrawLines(12f, new ColorRgba(Color.AntiqueWhite));
+        // game.Area.DrawLines(12f, new ColorRgba(Color.AntiqueWhite));
         game.MousePos.Draw(24f, new ColorRgba(Color.Lime), 36);
     }
 
     protected override void DrawUI(ScreenInfo ui)
     {
-        var center = ui.Area.ApplyMargins(0.1f, 0.1f, 0.1f, 0.1f);
-        center.Draw(new ColorRgba(Color.DarkSlateGray));
+        var area = ui.Area.ApplyMargins(0.01f, 0.01f, 0.01f, 0.01f);
+        var split = area.SplitV(0.75f);
+        var top = split.top;
+        var bottom = split.bottom;
+        var splitBottom = bottom.SplitV(0.5f);
+        
+        top.Draw(new ColorRgba(Color.DarkSlateGray));
 
+        top = top.ApplyMargins(0.01f, 0.01f, 0.01f, 0.01f);
         int rows = 4;
-        var rects = center.SplitV(rows);
+        var rects = top.SplitV(rows);
         
         var r = rects[0];
         var text = currentSavegameProfile.DisplayName;
@@ -152,6 +197,12 @@ public class MyGame : Game
         var chance = MathF.Round(currentSavegameProfile.Chance * 100f) / 100f;
         text = $"> Chance: {chance}%";
         textFont.DrawWord(text, r, new AnchorPoint(0f, 0.5f));
+
+        var pathRect = splitBottom.top.ApplyMargins(0.01f, 0.01f, 0.01f, 0.01f);
+        textFont.DrawWord($"Path: {BasicPath}", pathRect, new AnchorPoint(0.5f, 0.5f));
+        
+        var inputRect = splitBottom.bottom.ApplyMargins(0.01f, 0.01f, 0.01f, 0.01f);
+        textFont.DrawWord("Cycle [Space] | Rand [Q] | Reset [R]", inputRect, new AnchorPoint(0.5f, 1f));
     }
 }
 
@@ -159,10 +210,10 @@ public class SavegameBasic : SavegameObject
 {
     public string CurrentProfileName { get; set; }
 
-    public SavegameBasic(string profileName, string fileName)
+    public SavegameBasic(string currentProfileName)
     {
-        CurrentProfileName = profileName;
-        name = fileName;
+        CurrentProfileName = currentProfileName;
+        name = "basic.txt";
         version = "1.0.0";
     }
 }
