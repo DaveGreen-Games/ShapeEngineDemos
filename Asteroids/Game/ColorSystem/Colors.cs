d50803d670c6842936e0cf42f457eca5d985a1f8
Dave Green <34277803+SoloByte@users.noreply.github.com>
2024-09-13 13:57:19 +0200

    Colors improvements.

--- Asteroids/Game/ColorSystem/Colors.cs
+++ Asteroids/Game/ColorSystem/Colors.cs
@@ -5,57 +5,152 @@ namespace Asteroids.Game.ColorSystem;
 
 public static class Colors
 {
-    public static readonly PaletteColor Background = new PaletteColor(new ColorRgba(Color.DarkBlue));           //
-    public static readonly PaletteColor ShipMain = new PaletteColor(new ColorRgba(Color.DarkBlue));             //
-    public static readonly PaletteColor ShipSecondary = new PaletteColor(new ColorRgba(Color.DarkBlue));        //
-    public static readonly PaletteColor ShipSpecial = new PaletteColor(new ColorRgba(Color.DarkBlue));          //
-    public static readonly PaletteColor Hardshell = new PaletteColor(new ColorRgba(Color.DarkBlue));            //
-    public static readonly PaletteColor AsteroidMain = new PaletteColor(new ColorRgba(Color.DarkBlue));         //
-    public static readonly PaletteColor AsteroidSecondary = new PaletteColor(new ColorRgba(Color.DarkBlue));    //
-    public static readonly PaletteColor AsteroidSpecial = new PaletteColor(new ColorRgba(Color.DarkBlue));      //
+
+    #region Background
+
+    public static readonly PaletteColor BackgroundVeryDark = new PaletteColor(ColorRgba.FromHex("000302"));
+    public static readonly PaletteColor BackgroundDark = new PaletteColor(ColorRgba.FromHex("0A1C1A"));
+    public static readonly PaletteColor BackgroundMedium = new PaletteColor(ColorRgba.FromHex("021F1B"));
+    public static readonly PaletteColor BackgroundLight = new PaletteColor(ColorRgba.FromHex("0A3D36"));
+    public static readonly PaletteColor BackgroundSpecial = new PaletteColor(ColorRgba.FromHex("55c2b3"));
+
     
-    public static ColorRgba BackgroundColor => Background.ColorRgba;
-    public static ColorRgba ShipMainColor => ShipMain.ColorRgba;
-    public static ColorRgba ShipSecondaryColor => ShipSecondary.ColorRgba;
-    public static ColorRgba ShipSpecialColor => ShipSpecial.ColorRgba;
+    public static ColorRgba BackgroundVeryDarkColor => BackgroundVeryDark.ColorRgba;
+    public static ColorRgba BackgroundDarkColor => BackgroundDark.ColorRgba;
+    public static ColorRgba BackgroundMediumColor => BackgroundMedium.ColorRgba;
+    public static ColorRgba BackgroundLightColor => BackgroundLight.ColorRgba;
+    public static ColorRgba BackgroundSpecialColor => BackgroundSpecial.ColorRgba;
+    
+    #endregion
+    
+    #region Text
+    
+    public static readonly PaletteColor TextVeryDark= new PaletteColor(ColorRgba.FromHex("223805"));
+    public static readonly PaletteColor TextDark = new PaletteColor(ColorRgba.FromHex("4f800f"));
+    public static readonly PaletteColor TextMedium = new PaletteColor(ColorRgba.FromHex("5b9a09"));
+    public static readonly PaletteColor TextLight = new PaletteColor(ColorRgba.FromHex("dbfbb1"));
+    public static readonly PaletteColor TextSpecial = new PaletteColor(ColorRgba.FromHex("91e326"));
+    
+    public static ColorRgba TextVeryDarkColor => TextVeryDark.ColorRgba;
+    public static ColorRgba TextDarkColor => TextDark.ColorRgba;
+    public static ColorRgba TextMediumColor => TextMedium.ColorRgba;
+    public static ColorRgba TextLightColor => TextLight.ColorRgba;
+    public static ColorRgba TextSpecialColor => TextSpecial.ColorRgba;
+    
+    #endregion
+    
+    #region Special
+
+    public static readonly PaletteColor Experience = new PaletteColor(ColorRgba.FromHex("b0ebe4"));
+    public static readonly PaletteColor Resource = new PaletteColor(ColorRgba.FromHex("fbb1bb"));
+    public static readonly PaletteColor Hardshell = new PaletteColor(ColorRgba.FromHex("e5db8a"));
+    public static readonly PaletteColor Evade = new PaletteColor(ColorRgba.FromHex("6aa68e"));
+    public static readonly PaletteColor Stun = new PaletteColor(ColorRgba.FromHex("a989cc"));
+
+    public static ColorRgba ExperienceColor => Experience.ColorRgba;
+    public static ColorRgba ResourceColor => Resource.ColorRgba;
     public static ColorRgba HardshellColor => Hardshell.ColorRgba;
-    public static ColorRgba AsteroidMainColor => AsteroidMain.ColorRgba;
-    public static ColorRgba AsteroidSecondaryColor => AsteroidSecondary.ColorRgba;
+    public static ColorRgba EvadeColor => Evade.ColorRgba;
+    public static ColorRgba StunColor => Stun.ColorRgba;
+    
+    #endregion
+
+    #region Ship
+    
+    public static readonly PaletteColor ShipVeryDark = new PaletteColor(ColorRgba.FromHex("0d1031"));
+    public static readonly PaletteColor ShipDark = new PaletteColor(ColorRgba.FromHex("151c51"));
+    public static readonly PaletteColor ShipMedium = new PaletteColor(ColorRgba.FromHex("212d82"));
+    public static readonly PaletteColor ShipLight = new PaletteColor(ColorRgba.FromHex("5466eb"));
+    public static readonly PaletteColor ShipSpecial = new PaletteColor(ColorRgba.FromHex("081fcf"));
+   
+    public static ColorRgba ShipVeryDarkColor => ShipVeryDark.ColorRgba;
+    public static ColorRgba ShipDarkColor => ShipDark.ColorRgba;
+    public static ColorRgba ShipMediumColor => ShipMedium.ColorRgba;
+    public static ColorRgba ShipLightColor => ShipLight.ColorRgba;
+    public static ColorRgba ShipSpecialColor => ShipSpecial.ColorRgba;
+    
+    #endregion
+    
+    #region Asteroids
+    
+    public static readonly PaletteColor AsteroidVeryDark = new PaletteColor(ColorRgba.FromHex("301216"));
+    public static readonly PaletteColor AsteroidDark = new PaletteColor(ColorRgba.FromHex("4A0A11"));
+    public static readonly PaletteColor AsteroidMedium = new PaletteColor(ColorRgba.FromHex("870312"));
+    public static readonly PaletteColor AsteroidLight = new PaletteColor(ColorRgba.FromHex("B53847"));
+    public static readonly PaletteColor AsteroidSpecial = new PaletteColor(ColorRgba.FromHex("ed4a5d"));
+    
+    public static ColorRgba AsteroidVeryDarkColor => AsteroidVeryDark.ColorRgba;
+    public static ColorRgba AsteroidDarkColor => AsteroidDark.ColorRgba;
+    public static ColorRgba AsteroidMediumColor => AsteroidMedium.ColorRgba;
+    public static ColorRgba AsteroidLightColor => AsteroidLight.ColorRgba;
     public static ColorRgba AsteroidSpecialColor => AsteroidSpecial.ColorRgba;
+    #endregion
+    
+    
 
     public static readonly ColorPalette ColorPalette = new
     (
-        Background,
-        ShipMain,
-        ShipSecondary,
-        ShipSpecial,
+        BackgroundVeryDark,
+        BackgroundDark,
+        BackgroundMedium,
+        BackgroundLight,
+        BackgroundSpecial,
+        
+        TextVeryDark,
+        TextDark,
+        TextMedium,
+        TextLight,
+        TextSpecial,
+        
+        Experience,
+        Resource,
         Hardshell,
-        AsteroidMain,
-        AsteroidSecondary,
+        Evade,
+        Stun,
+        
+        ShipVeryDark,
+        ShipDark,
+        ShipMedium,
+        ShipLight,
+        ShipSpecial,
+        
+        AsteroidVeryDark,
+        AsteroidDark,
+        AsteroidMedium,
+        AsteroidLight,
         AsteroidSpecial
     );
 
     public static readonly ColorScheme Default =
     new(
-        Background.Clone(),
-        ShipMain.Clone(),
-        ShipSecondary.Clone(),
-        ShipSpecial.Clone(),
+        BackgroundVeryDark.Clone(),
+        BackgroundDark.Clone(),
+        BackgroundMedium.Clone(),
+        BackgroundLight.Clone(),
+        BackgroundSpecial.Clone(),
+        
+        TextVeryDark.Clone(),
+        TextDark.Clone(),
+        TextLight.Clone(),
+        TextMedium.Clone(),
+        TextSpecial.Clone(),
+        
+        Experience.Clone(),
+        Resource.Clone(),
         Hardshell.Clone(),
-        AsteroidMain.Clone(),
-        AsteroidSecondary.Clone(),
-        AsteroidSpecial.Clone()
-    );
-    
-    public static readonly ColorScheme Alternative =
-    new(
-        Background.Clone(),
-        ShipMain.Clone(),
-        ShipSecondary.Clone(),
+        Evade.Clone(),
+        Stun.Clone(),
+        
+        ShipVeryDark.Clone(),
+        ShipDark.Clone(),
+        ShipMedium.Clone(),
+        ShipLight.Clone(),
         ShipSpecial.Clone(),
-        Hardshell.Clone(),
-        AsteroidMain.Clone(),
-        AsteroidSecondary.Clone(),
+        
+        AsteroidVeryDark.Clone(),
+        AsteroidDark.Clone(),
+        AsteroidMedium.Clone(),
+        AsteroidLight.Clone(),
         AsteroidSpecial.Clone()
     );
 
