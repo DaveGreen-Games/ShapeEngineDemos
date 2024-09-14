056f53c4088d64d9398919c60d5158ae4b58c3dd
Dave Green <34277803+SoloByte@users.noreply.github.com>
2024-09-14 08:51:06 +0200

    GameScene spawns a ship now.

--- AsteroidsGalacticMayhem/GameSource/Scenes/GameScene.cs
+++ AsteroidsGalacticMayhem/GameSource/Scenes/GameScene.cs
@@ -1,14 +1,23 @@
 using AsteroidsGalacticMayhem.GameSource.ColorSystem;
+using AsteroidsGalacticMayhem.GameSource.Entities.Ships;
 using ShapeEngine.Core;
 using ShapeEngine.Core.Structs;
+using ShapeEngine.Lib;
 
 namespace AsteroidsGalacticMayhem.GameSource.Scenes;
 
 public class GameScene : Scene
 {
+    private Ship ship = new ShipGunslinger();
+    
+    
+    
     protected override void OnActivate(Scene oldScene)
     {
-        
+        ship = new ShipGunslinger();
+        ship.Spawn(new(), new(1, 0));
+        InitSpawnArea(-5000, -5000, 10000, 10000);
+        SpawnArea?.AddGameObject(ship);
     }
 
     protected override void OnDeactivate()
@@ -16,9 +25,15 @@ public class GameScene : Scene
         
     }
 
+    protected override void OnUpdate(GameTime time, ScreenInfo game, ScreenInfo gameUi, ScreenInfo ui)
+    {
+        
+    }
+
     protected override void OnDrawGame(ScreenInfo game)
     {
         Game.CurrentGameInstance.BackgroundColorRgba = Colors.BackgroundDarkColor;
+        SpawnArea?.Bounds.DrawLines(12f, Colors.BackgroundSpecialColor);
         
     }
 }
