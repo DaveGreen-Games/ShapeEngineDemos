9d169d11d4e243fbb17c8b7222b870c690eecf6d
Dave Green <34277803+SoloByte@users.noreply.github.com>
2024-10-07 21:18:01 +0200

    Experience & Resource class moved to their own files.

--- AsteroidsGalacticMayhem/GameSource/Entities/Collectibles/Collectible.cs
+++ AsteroidsGalacticMayhem/GameSource/Entities/Collectibles/Collectible.cs
@@ -1,6 +1,5 @@
 using System.Linq.Expressions;
 using System.Numerics;
-using AsteroidsGalacticMayhem.GameSource.ColorSystem;
 using ShapeEngine.Core;
 using ShapeEngine.Core.Collision;
 using ShapeEngine.Core.Shapes;
@@ -8,53 +7,6 @@ using ShapeEngine.Core.Structs;
 using ShapeEngine.Lib;
 
 namespace AsteroidsGalacticMayhem.GameSource.Entities.Collectibles;
-
-
-public class Experience : Collectible
-{
-    public Experience(Vector2 pos, float size) : base(pos, size)
-    {
-        
-    }
-
-    public override void DrawGame(ScreenInfo game)
-    {
-        Collider.GetCircleShape().DrawLines(4f, Colors.ExperienceColor);
-    }
-
-    public override void DrawGameUI(ScreenInfo gameUi)
-    {
-        
-    }
-}
-
-public class Resource : Collectible
-{
-    
-    public Resource(Vector2 pos, float size) : base(pos, size)
-    {
-        Tween = TweenType.BOUNCE_OUT;
-        Duration = 2f;
-        
-
-    }
-
-
-    public override void DrawGame(ScreenInfo game)
-    {
-        float thickness = ShapeMath.LerpFloat(2f, 6f, F);
-        var baseCircle = Collider.GetCircleShape();
-        var newRadius = ShapeMath.LerpFloat(baseCircle.Radius, baseCircle.Radius * 2, F);
-        var newCircle = baseCircle.SetRadius(newRadius);
-        newCircle.DrawLines(thickness, Colors.ResourceColor);
-    }
-
-    public override void DrawGameUI(ScreenInfo gameUi)
-    {
-        
-    }
-}
-
 
 public abstract class Collectible : CollisionObject // make abstract!
 {
---
+++ AsteroidsGalacticMayhem/GameSource/Entities/Collectibles/Experience.cs
@@ -0,0 +1,26 @@
+using System.Numerics;
+using AsteroidsGalacticMayhem.GameSource.ColorSystem;
+using ShapeEngine.Core.Structs;
+using ShapeEngine.Lib;
+
+namespace AsteroidsGalacticMayhem.GameSource.Entities.Collectibles;
+
+public class Experience : Collectible
+{
+    public Experience(Vector2 pos, float size) : base(pos, size)
+    {
+        Tween = TweenType.BOUNCE_OUT;
+        Duration = 2f;
+    }
+
+    public override void DrawGame(ScreenInfo game)
+    {
+        float thickness = ShapeMath.LerpFloat(2f, 6f, F);
+        var baseCircle = Collider.GetCircleShape();
+        var newRadius = ShapeMath.LerpFloat(baseCircle.Radius, baseCircle.Radius * 2, F);
+        var newCircle = baseCircle.SetRadius(newRadius);
+        newCircle.DrawLines(thickness, Colors.ResourceColor);
+    }
+
+    public override void DrawGameUI(ScreenInfo gameUi) { }
+}
